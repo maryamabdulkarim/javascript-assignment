85f2c67fe80fe513f03acc4b26a655992e5c034e
unknown <maryamabdulkarim06@gmail.com>
2023-05-12 07:08:03 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector("#myButton").onclick = function () {
+  const button = document.querySelector("pUpdate");
+  document.querySelector('.pUpdate').textContent = 'I am currently learning JavaScript';
+};
